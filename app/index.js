4ac4f68a150f549da12d352b63191fa99124626f
kajarenc <kajarenc@gmail.com>
2016-06-05 22:05:00 +0400

    lesson three complete

--- app/index.js
+++ app/index.js
@@ -1,16 +1,53 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
+var USER_DATA = {
+    name: "Maren Javadyan",
+    username: "kajarenc",
+    image: "https://avatars3.githubusercontent.com/u/6664805"
+};
+
+var ProfilePic = React.createClass({
+    render: function () {
+        return <img src={this.props.imageUrl} style={{height:100, width: 100}}/>
+    }
+});
+
+var ProfileLink = React.createClass({
     render: function () {
         return (
-            <div>Hello ReactJS program!</div>
+            <div>
+                <a href={'https://www.github.com/' + this.props.username}>
+                    {this.props.username}
+                </a>
+            </div>
         )
+    }
+});
 
+var ProfileName = React.createClass({
+    render: function () {
+        return (
+            <div>{this.props.name}</div>
+        )
+    }
+});
+
+
+var Avatar = React.createClass({
+    render: function () {
+        return (
+            <div>
+                <ProfilePic imageUrl={this.props.user.image}/>
+                <ProfileName name={this.props.user.name}/>
+                <ProfileLink username={this.props.user.username}/>
+
+            </div>
+        )
     }
 });
 
 ReactDOM.render(
-    <HelloWorld/>,
+    <Avatar user={USER_DATA}/>,
     document.getElementById('app')
-)
+);
