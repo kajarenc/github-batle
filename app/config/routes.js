672de1a93c1cd456447d42a263ef89f47d07989d
kajarenc <kajarenc@gmail.com>
2016-06-08 03:19:43 +0400

    video 6 complete (without life cycle understanding :( )

--- app/config/routes.js
+++ app/config/routes.js
@@ -7,15 +7,17 @@ var IndexRoute = ReactRouter.IndexRoute;
 var Main = require('../components/Main');
 var Home = require("../components/Home");
 var PromptContainer = require('../containers/PromptContainer');
+var ConfirmBattleContainer = require('../containers/ConfirmBattleContainer');
 
 var routes = (
-  <Router history={hashHistory}>
-    <Route path='/' component={Main}>
-      <IndexRoute component={Home} />
-      <Route path='playerOne' header='Player One' component={PromptContainer} />
-      <Route path='playerTwo/:playerOne' header='Player Two' component={PromptContainer} />
-    </Route>
-  </Router>
+    <Router history={hashHistory}>
+        <Route path='/' component={Main}>
+            <IndexRoute component={Home}/>
+            <Route path='playerOne' header='Player One' component={PromptContainer}/>
+            <Route path='playerTwo/:playerOne' header='Player Two' component={PromptContainer}/>
+            <Route path='battle' header='Player Two' component={ConfirmBattleContainer}/>
+        </Route>
+    </Router>
 );
 
 module.exports = routes;
---
+++ app/containers/ConfirmBattleContainer.js
@@ -0,0 +1,31 @@
+var React = require('react');
+var ConfirmBattle = require('../components/ConfirmBattle');
+
+var ConfirmBattleContainer = React.createClass({
+    contextTypes: {
+        router: React.PropTypes.object.isRequired
+    },
+    getInitialState: function () {
+        return {
+            isLoading: true,
+            playersInfo: []
+        }
+    },
+
+    componentDidMount: function () {
+        var query = this.props.location.query;
+        console.log('QUERY:', query);
+        // Fetch information from Github, and then update state
+    },
+
+    render: function () {
+        return (
+            <ConfirmBattle
+                isLoading={this.state.isLoading}
+                playersInfo={this.state.playersInfo}
+            />
+        )
+    }
+});
+
+module.exports = ConfirmBattleContainer;
